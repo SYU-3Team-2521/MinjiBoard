2b001ebd118505b3e27ae0f96d39a011edb34d4d
UiCHAN <why4622@naver.com>
2025-08-24 14:45:55 +0900

    0824 배포 코드 클론

--- src/main/java/syu/likealion3/hackathon/config/CorsConfig.java
+++ src/main/java/syu/likealion3/hackathon/config/CorsConfig.java
@@ -1,28 +1,24 @@
 package syu.likealion3.hackathon.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Value("${app.cors.allowed-origins:http://localhost:3000}")
-    private String allowedOrigins;
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                String[] origins = allowedOrigins.split("\\s*,\\s*");
-                registry.addMapping("/api/**")
-                        .allowedOrigins(origins)
-                        .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
-                        .allowCredentials(true);
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "https://minsaengcheck.syu-likelion.org",
+                        "http://localhost:5173",
+                        "http://127.0.0.1:5173",
+                        "http://15.164.228.20:5173"
+                )
+                .allowedHeaders("*")
+                .exposedHeaders("Location")
+                .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
